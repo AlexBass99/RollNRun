6a46438e98e4bc2115896141ff571a34093350fe
AlexBass99 <45899795+AlexBass99@users.noreply.github.com>
2022-10-25 19:20:30 +0200

    Revert "Producto -->HayStock(), RestarStock(), SumarStock() implementados"
    This reverts commit 5074153e11aa36b1445ba5d3d9af2ee18e7e6d5d.

--- Roll_n_RunGen/Roll_n_RunGenNHibernate/CEN/Roll_n_Run/ProductoCEN_hayStock.cs
+++ Roll_n_RunGen/Roll_n_RunGenNHibernate/CEN/Roll_n_Run/ProductoCEN_hayStock.cs
@@ -21,24 +21,12 @@ public partial class ProductoCEN
 {
 public bool HayStock (int p_oid, int p_cantidad)
 {
-            /*PROTECTED REGION ID(Roll_n_RunGenNHibernate.CEN.Roll_n_Run_Producto_hayStock) ENABLED START*/
+        /*PROTECTED REGION ID(Roll_n_RunGenNHibernate.CEN.Roll_n_Run_Producto_hayStock) ENABLED START*/
 
-           ProductoEN productoEN = new ProductoEN();
-            ProductoCEN productoCEN = new ProductoCEN();
-            productoEN = productoCEN.ReadOID(p_oid);
+        // Write here your custom code...
 
-            //La cantidad es el numero de ese producto que se quiere restar del stock total
-            //Comprobamos si la cantidad es MAYOR a cero y MENOR O IGUAL que el stock actual y se lo restamos
-            if (p_cantidad > 0 && p_cantidad <= productoEN.Stock)
-            {
-                return true;
-                //notificar que hay suficiente stock
-            }
-            else
-            {
-                return false;
-                //notificar en consola que no hay suficiente stock
-            }
+        throw new NotImplementedException ("Method HayStock() not yet implemented.");
+
         /*PROTECTED REGION END*/
 }
 }
--- Roll_n_RunGen/Roll_n_RunGenNHibernate/CEN/Roll_n_Run/ProductoCEN_sumarStock.cs
+++ Roll_n_RunGen/Roll_n_RunGenNHibernate/CEN/Roll_n_Run/ProductoCEN_sumarStock.cs
@@ -23,16 +23,6 @@ public void SumarStock (int p_oid, int p_cantidad)
 {
         /*PROTECTED REGION ID(Roll_n_RunGenNHibernate.CEN.Roll_n_Run_Producto_sumarStock) ENABLED START*/
 
-        ProductoEN productoEN = new ProductoEN();
-        ProductoCEN productoCEN = new ProductoCEN();
-        productoEN = productoCEN.ReadOID(p_oid);
-        if(p_cantidad>0){
-                productoEN.Stock = productoEN.Stock+p_cantidad;
-                //se avisa de que se ha sumado el stock
-        }else{
-                //se avisa de que no se ha podido sumar el stock
-        }
-
         // Write here your custom code...
 
         throw new NotImplementedException ("Method SumarStock() not yet implemented.");
