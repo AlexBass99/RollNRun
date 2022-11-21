7a9f9954d03d6c176ca1b456b4c8fe194aa53cb7
AlexBass99 <45899795+AlexBass99@users.noreply.github.com>
2022-11-21 15:04:29 +0100

    Actualizado Producto (Falta hacer funcionar los Enum en el Create)
    - Creado su Assembler y ViewModel
    - Creado su Controller
    - Creada la vista Index y su función en el Controller
    - Creada la vista Details y su función en el Controller
    - Creada la vista Create y su función en el Controller
    - Creada la vista Edit y su función en el Controller

--- Roll_n_RunGen/RollNRunWeb/Controllers/ProductoController.cs
+++ Roll_n_RunGen/RollNRunWeb/Controllers/ProductoController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using Roll_n_RunGenNHibernate.EN.Roll_n_Run;
 using Roll_n_RunGenNHibernate.CEN.Roll_n_Run;
+using Roll_n_RunGenNHibernate.CAD.Roll_n_Run;
+using RollNRunWeb.Assemblers;
+using RollNRunWeb.Models;
 
 namespace RollNRunWeb.Controllers
 {
@@ -13,16 +16,28 @@ namespace RollNRunWeb.Controllers
         // GET: Producto
         public ActionResult Index()
         {
-            ProductoCEN productoCEN = new ProductoCEN();
-            IList<ProductoEN> productos = productoCEN.ReadAll(0,20).ToList<ProductoEN>();
+            SessionInitialize();
+            ProductoCAD productoCAD = new ProductoCAD(session);
+            ProductoCEN productoCEN = new ProductoCEN(productoCAD);
 
-            return View(productos);
+            IList<ProductoEN> productosEN = productoCEN.ReadAll(0, -1);
+            IEnumerable<ProductoViewModel> productosViewModel = new ProductoAssembler().ConvertListENToModel(productosEN).ToList();
+            SessionClose();
+
+            return View(productosViewModel);
         }
 
         // GET: Producto/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            SessionInitialize();
+            ProductoCAD productoCAD = new ProductoCAD(session);
+            ProductoCEN productoCEN = new ProductoCEN(productoCAD);
+
+            ProductoEN productoEN = productoCEN.ReadOID(id);
+            ProductoViewModel productoViewModel = new ProductoAssembler().ConvertENToModelUI(productoEN);
+
+            return View(productoViewModel);
         }
 
         // GET: Producto/Create
@@ -33,11 +48,12 @@ namespace RollNRunWeb.Controllers
 
         // POST: Producto/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(ProductoViewModel prod)
         {
             try
             {
-                // TODO: Add insert logic here
+                ProductoCEN productoCEN = new ProductoCEN();
+                productoCEN.New_(prod.nombre, prod.marca, prod.stock, prod.precio, prod.imagen, prod.descripcion, 0, (Roll_n_RunGenNHibernate.Enumerated.Roll_n_Run.Tipo_productoEnum)prod.tipo_producto, prod.oferta);
 
                 return RedirectToAction("Index");
             }
@@ -50,16 +66,27 @@ namespace RollNRunWeb.Controllers
         // GET: Producto/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            SessionInitialize();
+            ProductoCAD productoCAD = new ProductoCAD(session);
+            ProductoCEN productoCEN = new ProductoCEN(productoCAD);
+
+            ProductoEN productoEN = productoCEN.ReadOID(id);
+            ProductoViewModel productoViewModel = new ProductoAssembler().ConvertENToModelUI(productoEN);
+
+            SessionClose();
+
+            return View(productoViewModel);
         }
 
         // POST: Producto/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, ProductoViewModel prod)
         {
             try
             {
-                // TODO: Add update logic here
+                ProductoCEN productoCEN = new ProductoCEN();
+                ProductoEN productoEN = productoCEN.ReadOID(id);
+                productoCEN.Modify(id, prod.nombre, prod.marca, prod.stock, prod.precio, prod.imagen, prod.descripcion, productoEN.Val_media, prod.tipo_producto, prod.oferta);
 
                 return RedirectToAction("Index");
             }
