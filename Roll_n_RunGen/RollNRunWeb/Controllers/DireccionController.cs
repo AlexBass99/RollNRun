eb9f8caa2dbc1a4a0cef66b9b8c3a614ad31e240
DeimosTricky <115697655+DeimosTricky@users.noreply.github.com>
2022-11-23 16:59:51 +0100

    Hecho casi todo Direccion
    Falta el delete
    
    Edit, Details y el Index funcionan perfectamente
    
    Hay un problema con el Create, lo he dejado en forma de anotación por si no me da tiempo a corregirlo más tarde

--- Roll_n_RunGen/RollNRunWeb/Controllers/DireccionController.cs
+++ Roll_n_RunGen/RollNRunWeb/Controllers/DireccionController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using Roll_n_RunGenNHibernate.EN.Roll_n_Run;
 using Roll_n_RunGenNHibernate.CEN.Roll_n_Run;
+using Roll_n_RunGenNHibernate.CAD.Roll_n_Run;
+using RollNRunWeb.Models;
+using RollNRunWeb.Assemblers;
 
 namespace RollNRunWeb.Controllers
 {
@@ -13,16 +16,30 @@ namespace RollNRunWeb.Controllers
         // GET: Direccion
         public ActionResult Index()
         {
-            DireccionCEN direccionCEN = new DireccionCEN();
-            IList<DireccionEN> direcciones = direccionCEN.ReadAll(0,20).ToList<DireccionEN>();
+            SessionInitialize();
 
-            return View(direcciones);
+            DireccionCAD dirCad = new DireccionCAD(session);
+            DireccionCEN dirCEN = new DireccionCEN(dirCad);
+
+            IList<DireccionEN> listEN = dirCEN.ReadAll(0, -1);
+            IEnumerable<DireccionViewModel> listViewModel = new DireccionAssembler().ConvertListENToModel(listEN).ToList();
+
+            SessionClose();
+
+            return View(listViewModel);
         }
 
         // GET: Direccion/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            SessionInitialize();
+            DireccionCAD dirCAD = new DireccionCAD(session);
+            DireccionCEN dirCEN = new DireccionCEN(dirCAD);
+
+            DireccionEN dirEN = dirCEN.ReadOID(id);
+            DireccionViewModel direccionViewModel = new DireccionAssembler().ConvertENToModelUI(dirEN);
+
+            return View(direccionViewModel);
         }
 
         // GET: Direccion/Create
@@ -33,13 +50,18 @@ namespace RollNRunWeb.Controllers
 
         // POST: Direccion/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(DireccionViewModel dir)
         {
             try
             {
                 // TODO: Add insert logic here
+                DireccionCEN dirCEN = new DireccionCEN();
+                dirCEN.New_(dir.Provincia, dir.Localidad, dir.CP, dir.Calle, 0); //el 0 debería ser la ID de un Usuario, como aun no
+                // hemos hecho Usuario lo he dejado como "0" para no crear un Usuario por si acaso
 
-                return RedirectToAction("Index");
+
+                return RedirectToAction("Index"); //No redirige, no se por qué, mi teoría es por no poner bien el Usuario
+                //pero no estoy seguro
             }
             catch
             {
@@ -50,16 +72,27 @@ namespace RollNRunWeb.Controllers
         // GET: Direccion/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            SessionInitialize();
+            DireccionCAD dirCAD = new DireccionCAD(session);
+            DireccionCEN dirCEN = new DireccionCEN(dirCAD);
+
+            DireccionEN dirEN = dirCEN.ReadOID(id);
+            DireccionViewModel direccionViewModel = new DireccionAssembler().ConvertENToModelUI(dirEN);
+
+            SessionClose();
+
+            return View(direccionViewModel);
         }
 
         // POST: Direccion/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
+        public ActionResult Edit(int id, DireccionViewModel dir)
+        { 
             try
             {
-                // TODO: Add update logic here
+                DireccionCEN dirCEN = new DireccionCEN();
+                DireccionEN dirEN = dirCEN.ReadOID(id);
+                dirCEN.Modify(id, dir.Provincia, dir.Localidad, dir.CP, dir.Calle);
 
                 return RedirectToAction("Index");
             }
