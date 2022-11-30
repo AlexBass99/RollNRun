e8c20d588c5e962705fee8a12c8a3ece72eb7d4f
jjaf2 <90913842+jjaf2@users.noreply.github.com>
2022-11-30 20:11:04 +0100

    Subforo y entrada casi terminados
    Arreglar en el assembler y el viewmodel de subforo los:
    	usuarios que lo siguen y las entradas
    
    Arreglar en el assembler de entrada: el null reference de productoEntrada
    
    Arreglar el tema del create de entrada:
    	la id del subforo aun no se de donde sacarla y no crea la entrada
    
    En el controller de subforo ver como pasar la fecha actual en el create

---
+++ Roll_n_RunGen/RollNRunWeb/Assemblers/SubforoAssembler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Roll_n_RunGenNHibernate.EN.Roll_n_Run;
+using RollNRunWeb.Models;
+
+namespace RollNRunWeb.Assemblers
+{
+    public class SubforoAssembler
+    {
+        public SubforoViewModel ConvertENToModelUI(SubforoEN en)
+        {
+            SubforoViewModel dir = new SubforoViewModel();
+            dir.id = en.Id;
+            //dir.usuario = (IList<int>)en.Usuarios;
+            //dir.entrada = (IList<int>)en.Entrada;
+            dir.Autor = en.Autor.Nickname;
+            dir.Titulo = en.Titulo;
+            dir.Fecha = (DateTime)en.Fecha;
+            dir.Descripcion = en.Descripcion;
+            dir.NumEntradas = en.NumEntradas;
+
+            return dir;
+        }
+
+        public IList<SubforoViewModel> ConvertListENToModel(IList<SubforoEN> glup)
+        {
+            IList<SubforoViewModel> dirs = new List<SubforoViewModel>();
+            foreach (SubforoEN ugh in glup)
+            {
+                dirs.Add(ConvertENToModelUI(ugh));
+            }
+            return dirs;
+        }
+    }
+}
---
+++ Roll_n_RunGen/RollNRunWeb/Controllers/SubforoController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Roll_n_RunGenNHibernate.EN.Roll_n_Run;
+using Roll_n_RunGenNHibernate.CEN.Roll_n_Run;
+using Roll_n_RunGenNHibernate.CAD.Roll_n_Run;
+using RollNRunWeb.Models;
+using RollNRunWeb.Assemblers;
+
+namespace RollNRunWeb.Controllers
+{
+    public class SubforoController : BasicController
+    {
+        // GET: Subforo
+        public ActionResult Index()
+        {
+            SessionInitialize();
+            SubforoCAD subforoCAD = new SubforoCAD(session);
+            SubforoCEN subforoCEN = new SubforoCEN(subforoCAD);
+
+            IList<SubforoEN> subforosEN = subforoCEN.ReadAll(0, -1);
+            IEnumerable<SubforoViewModel> subforoViewModel = new SubforoAssembler().ConvertListENToModel(subforosEN).ToList();
+            SessionClose();
+
+            return View(subforoViewModel);
+        }
+
+        // GET: Subforo/Details/5
+        public ActionResult Details(int id)
+        {
+            SessionInitialize();
+            SubforoCAD subforoCAD = new SubforoCAD(session);
+            SubforoCEN subforoCEN = new SubforoCEN(subforoCAD);
+
+            SubforoEN subforoEN = subforoCEN.ReadOID(id);
+            SubforoViewModel subforoViewModel = new SubforoAssembler().ConvertENToModelUI(subforoEN);
+
+            return View(subforoViewModel);
+        }
+
+        // GET: Subforo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Subforo/Create
+        [HttpPost]
+        public ActionResult Create(SubforoViewModel sforo)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                SubforoCEN subforoCEN = new SubforoCEN();
+                if (Session["Usuario"] != null)
+                {
+                    sforo.Autor = ((UsuarioEN)Session["Usuario"]).Nickname;
+                    subforoCEN.New_(((UsuarioEN)Session["Usuario"]).Id, sforo.Titulo, new DateTime(2022, 11, 20), sforo.Descripcion, 0);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Subforo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            SessionInitialize();
+            SubforoCAD subforoCAD = new SubforoCAD(session);
+            SubforoCEN subforoCEN = new SubforoCEN(subforoCAD);
+
+            SubforoEN subforoEN = subforoCEN.ReadOID(id);
+            SubforoViewModel subforoViewModel = new SubforoAssembler().ConvertENToModelUI(subforoEN);
+
+            SessionClose();
+
+            return View(subforoViewModel);
+        }
+
+        // POST: Subforo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, SubforoViewModel sforo)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                SubforoCEN subforoCEN = new SubforoCEN();
+                SubforoEN subforoEN = subforoCEN.ReadOID(id);
+                subforoCEN.Modify(id, sforo.Titulo, sforo.Fecha, sforo.Descripcion, sforo.NumEntradas);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Subforo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            SessionInitialize();
+            SubforoCAD subforoCAD = new SubforoCAD(session);
+            SubforoCEN subforoCEN = new SubforoCEN(subforoCAD);
+
+            SubforoEN subforoEN = subforoCEN.ReadOID(id);
+            SubforoViewModel subforoViewModel = new SubforoAssembler().ConvertENToModelUI(subforoEN);
+            SessionClose();
+
+            return View(subforoViewModel);
+        }
+
+        // POST: Subforo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, SubforoViewModel sforo)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                SubforoCEN subforoCEN = new SubforoCEN();
+                subforoCEN.Destroy(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
